f3f940557ad944599601a1835bf7ffe79a66c8f6
Yunseok-laptop <jng0820@naver.com>
2018-07-04 18:22:03 +0900

    minor change on laptop

--- validate.py
+++ validate.py
@@ -1,47 +1,35 @@
 import tensorflow as tf
 import numpy as np
-import train
 import Layer_class
 
 BATCH_SIZE = 1
 N_CLASSES = 2
-
 val_src = "images/validate/"
-filename = input()
+filename = "son.jpg"
 image_array = []
 image_array.append(val_src+filename)
 
+image_array = tf.cast(image_array,tf.float32)
+#image_array = tf.image.per_image_standardization(image_array)
+image_array = tf.reshape(image_array,[BATCH_SIZE,208,208,3])
 
-image = tf.cast(image_array, tf.float32)
-image = tf.image.per_image_standardization(image)
-image = tf.reshape(image, [1, 208, 208, 3])
+x = tf.placeholder(tf.float32, shape=[BATCH_SIZE, 208, 208, 3])
 
-logit = model1.inference(image, BATCH_SIZE, N_CLASSES)
+logs_train_dir = '/train/logs/'
 
-model1 = Layer_class.Model()
-
-logit = tf.nn.softmax(logit)
-
-x = tf.placeholder(tf.float32, shape=[208, 208, 3])
-
-logs_train_dir = '/home/kevin/tensorflow/cats_vs_dogs/logs/train/'
+sess = tf.Session()
 
 saver = tf.train.Saver()
+print("Reading checkpoints...")
+ckpt = tf.train.get_checkpoint_state(logs_train_dir)
+if ckpt and ckpt.model_checkpoint_path:
+    global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
+    saver.restore(sess, ckpt.model_checkpoint_path)
+    print('Loading success, global_step is %s' % global_step)
+else:
+    print('No checkpoint file found')
 
-    with tf.Session() as sess:
-
-        print("Reading checkpoints...")
-        ckpt = tf.train.get_checkpoint_state(logs_train_dir)
-        if ckpt and ckpt.model_checkpoint_path:
-            global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
-            saver.restore(sess, ckpt.model_checkpoint_path)
-            print('Loading success, global_step is %s' % global_step)
-        else:
-            print('No checkpoint file found')
-
-        prediction = sess.run(logit, feed_dict={x: image_array})
-        max_index = np.argmax(prediction)
-        if max_index==0:
-            print('This is a cat with possibility %.6f' %prediction[:, 0])
-        else:
-            print('This is a dog with possibility %.6f' %prediction[:, 1])
+logits = Layer_class._build_net(x, batch_size=BATCH_SIZE, n_classes=N_CLASSES, keep_prob=1)
+prediction = sess.run(logits, feed_dict={x: image_array})
+max_index = np.argmax(prediction)
+print("prediction :", max_index)
