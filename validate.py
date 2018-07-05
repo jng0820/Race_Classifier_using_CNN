c4654258037a8e1a46df80056faff00d7be735f6
jng0820@knu.ac.kr <dbstjr12>
2018-07-05 16:32:06 +0900

    validate_modify

--- validate.py
+++ validate.py
@@ -1,35 +1,41 @@
 import tensorflow as tf
 import numpy as np
 import Layer_class
+import matplotlib.pyplot as plt
+from PIL import Image
 
-BATCH_SIZE = 1
-N_CLASSES = 2
-val_src = "images/validate/"
-filename = "son.jpg"
-image_array = []
-image_array.append(val_src+filename)
+with tf.Graph().as_default():
+    BATCH_SIZE = 1
+    N_CLASSES = 3
+    val_src = 'images/validate/'
+    filename = 'saul.jpg'
+    filename2 = 'son.jpg'
 
-image_array = tf.cast(image_array,tf.float32)
-#image_array = tf.image.per_image_standardization(image_array)
-image_array = tf.reshape(image_array,[BATCH_SIZE,208,208,3])
+    image_file = Image.open(val_src + filename2)
+    image_file = image_file.resize([208, 208])
+    plt.imshow(image_file)
+    img_file1 = np.array(image_file)
 
-x = tf.placeholder(tf.float32, shape=[BATCH_SIZE, 208, 208, 3])
+    images = np.reshape(img_file1,newshape=[BATCH_SIZE,208,208,3])
 
-logs_train_dir = '/train/logs/'
+    x = tf.placeholder(tf.float32, shape=[BATCH_SIZE, 208, 208, 3])
+    keep_prob = tf.placeholder(tf.float32)
 
-sess = tf.Session()
+    logits = Layer_class._build_net(x, batch_size=BATCH_SIZE, n_classes=N_CLASSES, keep_prob=1)
+    logits = tf.nn.softmax(logits)
+    logs_train_dir = 'train/logs/'
 
-saver = tf.train.Saver()
-print("Reading checkpoints...")
-ckpt = tf.train.get_checkpoint_state(logs_train_dir)
-if ckpt and ckpt.model_checkpoint_path:
-    global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
-    saver.restore(sess, ckpt.model_checkpoint_path)
-    print('Loading success, global_step is %s' % global_step)
-else:
-    print('No checkpoint file found')
+    saver = tf.train.Saver()
+    with tf.Session() as sess:
+        print("Reading checkpoints...")
+        ckpt = tf.train.get_checkpoint_state(logs_train_dir)
+        if ckpt and ckpt.model_checkpoint_path:
+            global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
+            saver.restore(sess, ckpt.model_checkpoint_path)
+            print('Loading success, global_step is %s' % global_step)
+        else:
+            print('No checkpoint file found')
+        prediction = sess.run(logits, feed_dict={x: images, keep_prob : 1})
+        max_index = np.argmax(prediction)
 
-logits = Layer_class._build_net(x, batch_size=BATCH_SIZE, n_classes=N_CLASSES, keep_prob=1)
-prediction = sess.run(logits, feed_dict={x: image_array})
-max_index = np.argmax(prediction)
-print("prediction :", max_index)
+        print("prediction :", max_index)
